c9adc12265fc92d16ee30fb8cf9085e307c834d8
Aman Choudhary <142047102+dedJack@users.noreply.github.com>
2025-04-14 23:45:45 +0530

    Add secondnative repo (Day 5 - Day 14)

---
+++ secondNative/src/types.ts
@@ -0,0 +1,23 @@
+export type StackScreenTypeProp = {
+    "Home" : undefined;
+    "FlatList" :undefined;
+    "SectionList":undefined;
+    "TouchableScreen":undefined;
+    "ModelScreen" :undefined;
+    "PullToRefresh":undefined;
+    "AxiosDemo":undefined;
+    "FetchDemo":undefined;
+    "ThemeScreen":undefined;
+    "FadeDemoScreen":undefined;
+    "AnimationScreen":undefined;    
+    "InterpolateScreen":undefined;
+    "CombineAnimationScreen":undefined;
+    "GestureAnimationScreen":undefined;
+    "BasicReanimatedScreen":undefined;
+    "ReanimatedTypeScreen":undefined;
+    "GestureHandlerScreen":undefined;
+    "GesturePlayGroundScreen":undefined;
+    "ReanimationFormValidationScreen":undefined
+}
+
+
